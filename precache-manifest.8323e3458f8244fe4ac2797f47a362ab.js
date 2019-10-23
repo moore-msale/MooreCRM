e65fb998118e9e25790a2642f13df88197729af5
Erkin <mackinkenny@gmail.com>
2019-10-23 18:34:54 +0600

    react

---
+++ precache-manifest.8323e3458f8244fe4ac2797f47a362ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7281898e55193fc6b1cf21492fbcc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd957740f40b23b49728",
+    "url": "/static/css/main.62a06bc2.chunk.css"
+  },
+  {
+    "revision": "a04c06e18e52f15336f8",
+    "url": "/static/js/2.c5086397.chunk.js"
+  },
+  {
+    "revision": "fd957740f40b23b49728",
+    "url": "/static/js/main.bab03f6b.chunk.js"
+  },
+  {
+    "revision": "c5fd9c7245e7a1335c99",
+    "url": "/static/js/runtime-main.c61f3d44.js"
+  },
+  {
+    "revision": "1f91e94a1e49cfbf787d592c0cd13329",
+    "url": "/static/media/call.1f91e94a.svg"
+  },
+  {
+    "revision": "da9b75fbb8dfd70d309448f8febeac32",
+    "url": "/static/media/customer.da9b75fb.svg"
+  },
+  {
+    "revision": "3815faf5ad9afa84e00984976ff03fab",
+    "url": "/static/media/edit-task.3815faf5.svg"
+  },
+  {
+    "revision": "3e9e8f739fcf8e7814160ffbdebfeab5",
+    "url": "/static/media/graph.3e9e8f73.svg"
+  },
+  {
+    "revision": "ddb058c7d5a902c2fdc762f1ac734e65",
+    "url": "/static/media/home.ddb058c7.svg"
+  },
+  {
+    "revision": "b2975fe0ae4301cd11f3e36e3b6a2c1a",
+    "url": "/static/media/partnership.b2975fe0.svg"
+  }
+]);
